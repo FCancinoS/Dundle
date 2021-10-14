cb126b6c345301b02322fd621802028e005cd3b2
francisco-cancino <francisco.cancino@outlook.es>
2021-10-14 10:32:54 -0500

    Analizador de lexico y sintaxis

---
+++ dundle.py
@@ -0,0 +1,166 @@
+import ply.yacc as yacc
+import sys
+
+from lexer import tokens
+from lexer import analizador
+
+
+
+def p_DUNDLE(p):
+    'DUNDLE : FUNCTIONS MEIN'
+def p_MEIN(p):
+    'MEIN : MAIN LPAREN RPAREN LKEY M RKEY'
+def p_FUNCTIONS(p):
+    '''
+    FUNCTIONS : FUN ID LPAREN RPAREN LKEY M RKEY FUNCTIONS 
+              | 
+    '''
+def p_M(p): 
+    '''
+    M : AUX1 M             
+     |
+    '''
+
+def p_AUX1(p):
+    '''
+    AUX1 : DECLARACION
+        | LLAMAFUN
+        | IMPRESION
+        | CONDICIONALIF
+        | CONDICIONALLETIF
+        | CICLOWHILE
+        | CICLOLOOP
+        | CICLOFOR
+        | MATASSIG
+        | MATOPE
+    '''
+def p_DECLARACION(p):
+    '''
+    DECLARACION : LET ID
+               | LET ID IGUAL READ LPAREN RPAREN
+               | LET ID IGUAL EXPRESION
+               | LET ID  DIMENSION
+    '''
+def p_LLAMAFUN(p):
+    '''
+    LLAMAFUN : ID LPAREN RPAREN
+                | ID DIMENSION
+    '''
+def p_DIMENSION(p):
+    '''
+    DIMENSION : LCOR CONSTANTE RCOR 
+             | LCOR CONSTANTE RCOR LCOR CONSTANTE RCOR
+             | LCOR CONSTANTE RCOR LCOR CONSTANTE RCOR LCOR CONSTANTE RCOR
+    '''
+def p_IMPRESION(p):
+    '''
+    IMPRESION : PRINT LPAREN ID RPAREN
+             | PRINT LPAREN  STRING  RPAREN
+    '''
+def p_CONDICIONALIF(p):
+    '''
+    CONDICIONALIF : IF LPAREN C RPAREN LKEY M RKEY
+                  | IF LPAREN C RPAREN LKEY M RKEY ELSE LKEY M RKEY
+                  | IF LPAREN MATCOMP RPAREN LKEY M RKEY 
+                  | IF LPAREN MATCOMP RPAREN LKEY M RKEY ELSE LKEY M RKEY
+    '''
+def p_MATCOMP(p):
+    '''
+    MATCOMP : ID PUNTO ROWS EQUAL ID PUNTO ROWS AND ID PUNTO COLUMNS EQUAL ID PUNTO COLUMNS
+    '''
+
+def p_CONDICIONALLETLIF(p):
+    '''
+    CONDICIONALLETIF : LET ID IGUAL IF LPAREN C RPAREN LKEY CONSID RKEY ELSE LKEY CONSID RKEY
+    
+    '''
+
+def p_CONSID(p):
+    '''
+    CONSID : CONSTANTE
+          | ID
+    '''
+def p_CICLOWHILE(p):
+    '''
+    CICLOWHILE : WHILE LPAREN C RPAREN LKEY M RKEY 
+    '''
+def p_CICLOLOOP(p):
+    '''
+    CICLOLOOP : LOOP LKEY M RKEY
+             | LOOP LKEY M BREAK RKEY
+
+    '''
+def p_CICLOFOR(p):
+    '''
+    CICLOFOR : FOR LPAREN ID IN ID PUNTO CORROWS RPAREN LKEY M RKEY
+    '''
+def p_CORROWS(p):
+    '''
+    CORROWS : COLUMNS
+            | ROWS
+    '''
+def p_ANOR(p):
+    '''
+    ANOR : AND
+        | OR
+    '''
+def p_C(p):
+    '''
+    C : CONSID SIMBOLS CONSID 
+      | CONSID SIMBOLS CONSID ANOR C
+    '''
+def p_SIMBOLS(p):
+    '''
+    SIMBOLS : MENOR
+           | EQUAL
+           | MAYOR
+           | NOTEQUAL
+           | MENORIGUAL
+           | MAYORIGUAL
+    '''
+def p_EXPRESION(p):
+    '''
+    EXPRESION : T
+             | T PLUS T
+             | T MINUS T
+    '''
+def p_T(p):
+    '''
+    T : F
+     | F MULT
+     | F DIVIDE
+     |
+    '''
+def p_F(p):
+    '''
+    F : ID
+     | CONSTANTE
+     | LPAREN EXPRESION RPAREN
+    '''
+def p_MATASSIG(p):
+    '''
+    MATASSIG : ID LCOR CONSTANTE RCOR LCOR CONSTANTE RCOR IGUAL CONSTANTE 
+    '''
+def p_MATOPE(p):
+    '''
+    MATOPE : ID LCOR ID RCOR LCOR ID RCOR IGUAL ID LCOR ID RCOR LCOR ID RCOR OPS ID LCOR ID RCOR LCOR ID RCOR
+              '''    
+def p_OPS(p):
+    '''
+        OPS : PLUS
+             | MINUS
+             | MULT
+             | DIVIDE
+    '''
+def p_error(t):
+    if t:
+        resultado = "Error sintactico de tipo {} en el valor {} en la linea {} y posicion {}".format(str(t.type), str(t.value),str(t.lineno),str(t.lexpos) )
+        print(resultado)
+    
+parser = yacc.yacc()
+
+if __name__ == '__main__':
+
+    with open('suma.txt','r',encoding='utf8') as programa: 
+        mein = programa.read()
+    parser.parse(mein)
---
+++ lexer.py
@@ -0,0 +1,154 @@
+import ply.lex as lex
+import sys
+
+#Palabras reservadas
+resultado_lexema = []
+
+with open('main.txt','r',encoding='utf8') as programa: 
+    mein = programa.read()
+
+reserved ={
+    'if': 'IF',
+    'else':'ELSE',
+    'main':'MAIN',
+    'let' :'LET',
+    'for':'FOR',
+    'while':'WHILE',
+    'loop':'LOOP',
+    'print':'PRINT',
+    'read' : 'READ',
+    'break':'BREAK',
+    'in': 'IN',
+    'fun': 'FUN',
+    'rows':'ROWS',
+    'columns':'COLUMNS'
+}
+
+tokens =[
+    'PLUS','MINUS','DIVIDE','MULT','MENOR','MAYOR','IGUAL','EQUAL',
+    'MENORIGUAL','MAYORIGUAL','CONSTANTE','ID','LPAREN',
+    'RPAREN','NOTEQUAL','LCOR','RCOR',
+    'AND','OR','STRING','RKEY','LKEY','PUNTO','COMI'
+]+list(reserved.values())
+
+#DEFINICON DE LOS TOKENS
+t_AND = r'\&\&'
+t_OR = r'\|{2}'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_DIVIDE = r'/'
+t_MENOR = r'<'
+t_MAYOR = r'>'
+t_IGUAL = r'='
+t_MULT = r'\*'
+t_LPAREN =r'\('
+t_RPAREN =r'\)'
+#t_NOT = r'\!'
+t_LCOR = r'\['
+t_RCOR = r'\]'
+t_ignore  = ' \t'
+t_LKEY =r'\{'
+t_RKEY =r'\}'
+t_PUNTO =r'\.'
+t_COMI =r'\"'
+
+
+
+def t_EQUAL(t):
+    r'=='
+    return t
+
+def t_MENORIGUAL(t):
+    r'<='
+    return t
+
+def t_CONDOUBLE(t):
+    r'(\d+|(\-)\d+)(\.)(\d+)'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Double value too large %d", t.value)
+        t.value = 0.0
+    return t
+
+def t_MAYORIGUAL(t):
+    r'>='
+    return t
+
+def t_CONSTANTE(t):
+    r'(\d+)|(\-)\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z][a-zA-Z_0-9]*'
+   #r'\w(\w|\d)*(_(\d|\w)+)*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+def t_NOTEQUAL(t):
+    r'!='
+    return t
+
+def t_PLUSPLUS(t):
+    r'\+\+'
+    return t
+
+def t_MINMIN(t):
+    r'\-\-'
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+#def t_COMMENT(t):
+#     r'(/\*(.|\n)*?\*/)|(//.*)'
+#     pass
+
+
+def t_STRING(t):
+    r'\"(.)*?\"'
+    t.type = reserved.get(t.value,'STRING')
+    return t
+
+
+def t_comments(t):
+    r'/\*(.|\n)*?\*/'
+    t.lexer.lineno += t.value.count('\n')
+
+def prueba(data):
+    global resultado_lexema
+
+    analizador = lex.lex()
+    analizador.input(data)
+
+    resultado_lexema.clear()
+    while True:
+        tok = analizador.token()
+        if not tok:
+            break
+        # print("lexema de "+tok.type+" valor "+tok.value+" linea "tok.lineno)
+        estado = "Linea {:4} Tipo {:16} Valor {:16} Posicion {:4}".format(str(tok.lineno),str(tok.type) ,str(tok.value), str(tok.lexpos) )
+        resultado_lexema.append(estado)
+    return resultado_lexema
+
+ # instanciamos el analizador lexico
+analizador = lex.lex()
+
+if __name__ == '__main__':
+    #while True:
+        #data = input("ingrese: ")
+    #data = programa.read()
+    prueba(mein)
+    #print(mein)
+    for i in range(0, len(resultado_lexema)):
+        print(resultado_lexema[i] + "\n")
